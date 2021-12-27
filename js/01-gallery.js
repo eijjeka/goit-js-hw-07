97734f4153cf47bf0831c14add8f725bc3fa6267
eijjeka <eijjeka@gmail.com>
2021-12-27 13:50:56 +0200

    task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const parentEl = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) =>
+`<div class="gallery__item">
+<a class="gallery__link" href="${original}"
+onclick="event.preventDefault()">
+<img
+class="gallery__image"
+src="${preview}"
+data-source="${original}"
+alt="${description}"
+/>
+</a>
+</div>`).join('');
+
+parentEl.innerHTML = markup;
+
+
+const onGalleryItem = event => {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`).show()
+
+}
+
+const onClickEscape = event => {
+    if (event.key !== 'Escape') {
+        return
+    }
+    const div = document.querySelector('.basicLightbox');
+    div.classList.remove('basicLightbox--visible');
+}
+parentEl.addEventListener('click', onGalleryItem);
+window.addEventListener('keydown', onClickEscape);
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const parentEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markup = galleryItems.map(({ preview, original, description }) =>
+    `<a class="gallery__item" href="${original}" onclick="event.preventDefault()">
+  <img class="gallery__image" src="${preview}"  alt="Image description" title = "${description}" />
+</a>`).join('');
+
+parentEl.innerHTML = markup;
+
+let lightbox =  new SimpleLightbox('.gallery a', { /* options */ });
