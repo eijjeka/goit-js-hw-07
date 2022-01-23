355d1dfdbbb3f2ca60760c7e8769aafd544f448d
eijjeka <eijjeka@gmail.com>
2022-01-23 23:40:55 +0200

    update task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -44,6 +44,44 @@ function onImgContainerClick(event) {
     }
     
     parentEl.addEventListener('click', onImgContainerClick);
+
+////////////////////////
+
+// Change code below this line
+// console.log(galleryItems);
+
+// const mainGalleryList = document.querySelector('.gallery');
+
+// mainGalleryList.insertAdjacentHTML('beforeend', createGalleryItemsMarkup(galleryItems));
+
+
+// function createGalleryItemsMarkup(items) {
+//     return items.map(({ preview, original, description }) => {
+//         return `
+//         <div class="gallery__item">
+//         <a class="gallery__link" href="${original}" onclick="event.preventDefault()">
+//         <img
+//         class="gallery__image"
+//         src="${preview}"
+//         data-source="${original}"
+//         alt="${description}"
+//         />
+//         </a>
+//         </div>
+//         `}).join('');
+// }
+    
+//     mainGalleryList.addEventListener('click', onImageClick);
+
+// function onImageClick(event) {
+//     if (event.target.nodeName !== 'IMG') {
+//         return;
+//     }
+//     const instance = basicLightbox.create(`
+//     <img src="${event.target.dataset.source}" width="800" height="600">
+//     `);
+//     instance.show();
+// }
     
     // const onGalleryItem = event => {
     //     if (event.target.nodeName !== 'IMG') {
